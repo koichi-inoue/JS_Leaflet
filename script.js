323c119b38e729dd72d2fadf70a4f7b8ad5516ae
koichi-inoue <inoue.ko@gmail.com>
2021-12-02 12:46:18 +0900

    Update script.js

--- script.js
+++ script.js
@@ -6,17 +6,19 @@ window.onload = function() {
   // 中心の緯度・経度とズームレベルを指定
   map.setView([35, 136], 5);
 
-  //表示するタイルレイヤのURLとAttributionコントロールの記述を設定して、地図に追加する
+  //表示するタイルレイヤのURLとAttributionコントロールを設定
   L.tileLayer(
-
-    'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png' ,
-    // 'https://cyberjapandata.gsi.go.jp/xyz/std/{z}/{x}/{y}.png',
-    {
+    // OpenStreetMap を利用
+    'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png' , {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      //attribution: "<a href='https://maps.gsi.go.jp/development/ichiran.html' target='_blank'>地理院タイル</a>"
-    }
+  }).addTo(map);
 
-  ).addTo(map);
+  /* 地理院タイル を利用する場合
+  L.tileLayer(
+     'https://cyberjapandata.gsi.go.jp/xyz/std/{z}/{x}/{y}.png',{
+        attribution: "<a href='https://maps.gsi.go.jp/development/ichiran.html' target='_blank'>地理院タイル</a>"
+  }).addTo(map);
+  */
 
   L.marker([ 33.66983627668292, 130.44487416744232 ] ).addTo(map)
     .bindPopup('Kyushu Sangyo University')
